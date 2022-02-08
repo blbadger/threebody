6843335b8ece503a5b0fe1585894271ebd8d82c3
blbadger <blbadger10@gmail.com>
2022-02-07 21:43:35 -0500

    converted arrays to torch.Tensor objects

--- divergence.py
+++ divergence.py
@@ -13,18 +13,24 @@ import numexpr as ne
 import time
 import torch
 
-
 class Threebody:
 
-	def __init__(self, time_steps):
+	def __init__(self, time_steps, x_res, y_res):
+		self.x_res = x_res
+		self.y_res = y_res
 		self.distance = 0.5
 		self.m1 = 10
 		self.m2 = 20
 		self.m3 = 30
 		self.time_steps = time_steps
+		self.p1, self.p2, self.p3 = np.ndarray([]), np.ndarray([]), np.ndarray([])
+		self.v1, self.v2, self.v3 = np.ndarray([]), np.ndarray([]), np.ndarray([])
+		self.p1_prime, self.p2_prime, self.p3_prime = np.ndarray([]), np.ndarray([]), np.ndarray([])
+		self.v1_prime, self.v2_prime, self.v3_prime = np.ndarray([]), np.ndarray([]), np.ndarray([])
 
-		# assign a small time step
+		# assign a small number to each time step
 		self.delta_t = 0.001
+
 
 	def accelerations(self, p1, p2, p3):
 		"""
@@ -68,26 +74,46 @@ class Threebody:
 			bool_arr: np.ndarray[bool]
 
 		"""
+
 		separation_arr = np.sqrt((p1[0] - p1_prime[0])**2 + (p1[1] - p1_prime[1])**2 + (p1[2] - p1_prime[2])**2)
 		bool_arr = separation_arr <= self.distance
+
 		return bool_arr
 
 
-	def sensitivity(self, y_res, x_res):
+	def plot_divergence(self, divergence_array):
 		"""
-		Plots the sensitivity to initial values per starting point of planet 1, as
-		measured by the time until divergence.
+		Generates a plot of a divergence array
 
 		Args:
-			None
+			divergence_array: np.ndarray[float]
 
 		Returns:
+			None (saves pyplot.imshow() image)
+
+		"""
+		
+		plt.style.use('dark_background')
+		plt.imshow(divergence_array, cmap='inferno')
+		plt.axis('off')
+		plt.savefig('Threebody_divergence{0:04d}.png'.format(i//100), bbox_inches='tight', pad_inches=0, dpi=420)
+		plt.close()
+		return
+
+
+	def initialize_arrays(self, double_type=True):
+		"""
+		Initialize torch.Tensor arrays
+
+		kwargs:
+			double_type: bool, if True then tensors are of type torch.float64 else float32
+
+		returns:
 			None
 
 		"""
 
-		delta_t = self.delta_t
-		y, z = np.arange(-20, 20, 40/y_res), np.arange(-30, 30, 60/x_res)
+		y, z = np.arange(-20, 20, 40/self.y_res), np.arange(-30, 30, 60/self.x_res)
 		grid = np.meshgrid(z, y)
 		grid2 = np.meshgrid(z, y)
 
@@ -96,61 +122,80 @@ class Threebody:
 
 		# shift the grid by a small amount
 		grid2 = grid2[0] + 1e-3, grid2[1] + 1e-3
+
 		# grid of all -11, identical starting z-values
 		x_prime = np.zeros(grid[0].shape) - 10 + 1e-3
 
-		time_array = np.zeros(grid[0].shape)
-
 		# starting coordinates for planets
-		# p1_start = x_1, y_1, z_1
 		p1 = np.array([x, grid[0], grid[1]])
 		v1 = np.array([np.ones(grid[0].shape) * -3, np.zeros(grid[0].shape), np.zeros(grid[0].shape)])
 
-		# p2_start = x_2, y_2, z_2
 		p2 = np.array([np.zeros(grid[0].shape), np.zeros(grid[0].shape), np.zeros(grid[0].shape)])
 		v2 = np.array([np.zeros(grid[0].shape), np.zeros(grid[0].shape), np.zeros(grid[0].shape)])
 
-		# p3_start = x_3, y_3, z_3
 		p3 = np.array([np.ones(grid[0].shape) * 10, np.ones(grid[0].shape) * 10, np.ones(grid[0].shape) * 12])
 		v3 = np.array([np.ones(grid[0].shape) * 3, np.zeros(grid[0].shape), np.zeros(grid[0].shape)])
 
 		# starting coordinates for planets shifted
-		# p1_start = x_1, y_1, z_1
 		p1_prime = np.array([x_prime, grid2[0], grid2[1]])
 		v1_prime = np.array([np.ones(grid[0].shape) * -3, np.zeros(grid[0].shape), np.zeros(grid[0].shape)])
 
-		# p2_start = x_2, y_2, z_2
 		p2_prime = np.array([np.zeros(grid[0].shape), np.zeros(grid[0].shape), np.zeros(grid[0].shape)])
 		v2_prime = np.array([np.zeros(grid[0].shape), np.zeros(grid[0].shape), np.zeros(grid[0].shape)])
 
-		# p3_start = x_3, y_3, z_3
 		p3_prime = np.array([np.ones(grid[0].shape) * 10, np.ones(grid[0].shape) * 10, np.ones(grid[0].shape) * 12])
 		v3_prime = np.array([np.ones(grid[0].shape) * 3, np.zeros(grid[0].shape), np.zeros(grid[0].shape)])
 
-		# bool array of all True
-		still_together = grid[0] < 1e10
-		t = time.time()
+		# convert numpy arrays to torch.Tensor([float64]) objects (2x speedup)
 		p1, p2, p3 = torch.Tensor(p1), torch.Tensor(p2), torch.Tensor(p3)
 		v1, v2, v3 = torch.Tensor(v1), torch.Tensor(v2), torch.Tensor(v3)
-		p1, p2, p3 = p1.type(torch.double), p2.type(torch.double), p3.type(torch.double)
-		v1, v2, v3 = v1.type(torch.double), v2.type(torch.double), v3.type(torch.double)
-
 		p1_prime, p2_prime, p3_prime = torch.Tensor(p1_prime), torch.Tensor(p2_prime), torch.Tensor(p3_prime)
 		v1_prime, v2_prime, v3_prime = torch.Tensor(v1_prime), torch.Tensor(v2_prime), torch.Tensor(v3_prime)
 
+		if double_type:
+			ttype = torch.double
+		else:
+			ttype = torch.float
+
+		self.p1, self.p2, self.p3 = p1.type(ttype), p2.type(ttype), p3.type(ttype)
+		self.v1, self.v2, self.v3 = v1.type(ttype), v2.type(ttype), v3.type(ttype)
+		self.p1_prime, self.p2_prime, self.p3_prime = p1_prime.type(ttype), p2_prime.type(ttype), p3_prime.type(ttype)
+		self.v1_prime, self.v2_prime, self.v3_prime = v1_prime.type(ttype), v2_prime.type(ttype), v3_prime.type(ttype)
+
+		return
+
+
+	def sensitivity(self, iterations_video=False):
+		"""
+		Determine the sensitivity to initial values per starting point of planet 1, as
+		measured by the time until divergence.
+
+		kwargs:
+			iterations_video: Bool, if True then divergence is plotted every 100 time steps
+
+		Returns:
+			time_array: np.ndarray[int] of iterations until divergence
+
+		"""
+
+		delta_t = self.delta_t
+		self.initialize_arrays(double_type=True)
+		time_array = np.zeros(self.p1[0].shape)
+
+		# bool array of all True
+		still_together = time_array < 1e10
+
+		t = time.time()
 		# evolution of the system
 		for i in range(self.time_steps):
 			if i % 100 == 0:
 				print (i)
 				print (f'Elapsed time: {time.time() - t} seconds')
 				time_array2 = i - time_array 
-				plt.style.use('dark_background')
-				plt.imshow(time_array2, cmap='inferno')
-				plt.axis('off')
-				plt.savefig('Threebody_divergence{0:04d}.png'.format(i//100), bbox_inches='tight', pad_inches=0, dpi=420)
-				plt.close()
+				if iterations_video:
+					self.plot_divergence(time_array2)
 
-			not_diverged = self.not_diverged(p1, p1_prime)
+			not_diverged = self.not_diverged(self.p1, self.p1_prime)
 			not_diverged = not_diverged.numpy()
 
 			# points still together are not diverging now and have not previously
@@ -159,29 +204,30 @@ class Threebody:
 			# apply boolean mask to ndarray time_array
 			time_array[still_together] += 1
 
-			# calculate derivatives
-			dv1, dv2, dv3 = self.accelerations(p1, p2, p3)
-			dv1_prime, dv2_prime, dv3_prime = self.accelerations(p1_prime, p2_prime, p3_prime)
+			# calculate derivatives and store as class variables self.p1 ...
+			dv1, dv2, dv3 = self.accelerations(self.p1, self.p2, self.p3)
+			dv1_prime, dv2_prime, dv3_prime = self.accelerations(self.p1_prime, self.p2_prime, self.p3_prime)
 
-			nv1 = v1 + dv1 * delta_t
-			nv2 = v2 + dv2 * delta_t
-			nv3 = v3 + dv3 * delta_t
+			nv1 = self.v1 + dv1 * delta_t
+			nv2 = self.v2 + dv2 * delta_t
+			nv3 = self.v3 + dv3 * delta_t
 
-			p1 = p1 + v1 * delta_t
-			p2 = p2 + v2 * delta_t
-			p3 = p3 + v3 * delta_t
-			v1, v2, v3 = nv1, nv2, nv3
+			self.p1 = self.p1 + self.v1 * delta_t
+			self.p2 = self.p2 + self.v2 * delta_t
+			self.p3 = self.p3 + self.v3 * delta_t
+			self.v1, self.v2, self.v3 = nv1, nv2, nv3
 
-			nv1_prime = v1_prime + dv1_prime * delta_t
-			nv2_prime = v2_prime + dv2_prime * delta_t
-			nv3_prime = v3_prime + dv3_prime * delta_t
+			nv1_prime = self.v1_prime + dv1_prime * delta_t
+			nv2_prime = self.v2_prime + dv2_prime * delta_t
+			nv3_prime = self.v3_prime + dv3_prime * delta_t
 
-			p1_prime = p1_prime + v1_prime * delta_t
-			p2_prime = p2_prime + v2_prime * delta_t
-			p3_prime = p3_prime + v3_prime * delta_t
-			v1_prime, v2_prime, v3_prime = nv1_prime, nv2_prime, nv3_prime
+			self.p1_prime = self.p1_prime + self.v1_prime * delta_t
+			self.p2_prime = self.p2_prime + self.v2_prime * delta_t
+			self.p3_prime = self.p3_prime + self.v3_prime * delta_t
+			self.v1_prime, self.v2_prime, self.v3_prime = nv1_prime, nv2_prime, nv3_prime
 
 		return time_array
+
 
 	def three_body_phase(self):
 		"""
@@ -191,7 +237,7 @@ class Threebody:
 			None
 
 		Returns:
-			None
+			None (saves matplotlib.pyplot object image)
 
 		"""
 
@@ -247,13 +293,10 @@ class Threebody:
 
 		# starting point
 		p1[0], p2[0], p3[0] = p1_start, p2_start, p3_start
-
 		v1[0], v2[0], v3[0] = v1_start, v2_start, v3_start
 
 		p1_prime[0], p2_prime[0], p3_prime[0] = p1_start_prime, p2_start_prime, p3_start_prime
-
 		v1_prime[0], v2_prime[0], v3_prime[0] = v1_start_prime, v2_start_prime, v3_start_prime
-
 		time = [0]
 
 		# evolution of the system
@@ -279,73 +322,75 @@ class Threebody:
 			p2_prime[i + 1] = p2_prime[i] + v2_prime[i] * delta_t
 			p3_prime[i + 1] = p3_prime[i] + v3_prime[i] * delta_t
 
-			# if i % 1000 == 0:
-			# 	fig = plt.figure(figsize=(10, 10))
-			# 	ax = fig.gca(projection='3d')
-			# 	plt.gca().patch.set_facecolor('black')
-			# 	ax.set_xlim([-50, 300])
-			# 	ax.set_ylim([-10, 30])
-			# 	ax.set_zlim([-30, 70])
+			if i % 1000 == 0:
+				fig = plt.figure(figsize=(10, 10))
+				ax = fig.gca(projection='3d')
+				plt.gca().patch.set_facecolor('black')
+				ax.set_xlim([-50, 300])
+				ax.set_ylim([-10, 30])
+				ax.set_zlim([-30, 70])
 
-			# 	# plt.plot([i[0] for i in p1], [j[1] for j in p1], [k[2] for k in p1] , '^', color='red', lw = 0.05, markersize = 0.01, alpha=0.5)
-			# 	plt.plot([i[0] for i in p2], [j[1] for j in p2], [k[2] for k in p2] , '^', color='white', lw = 0.05, markersize = 0.01, alpha=0.5)
-			# 	plt.plot([i[0] for i in p3], [j[1] for j in p3], [k[2] for k in p3] , '^', color='blue', lw = 0.05, markersize = 0.01, alpha=0.5)
-			# 	# plt.plot([i[0] for i in p1_prime], [j[1] for j in p1_prime], [k[2] for k in p1_prime], '^', color='blue', lw=0.05, markersize=0.01, alpha=0.5)
+				plt.plot([i[0] for i in p1], [j[1] for j in p1], [k[2] for k in p1] , '^', color='red', lw = 0.05, markersize = 0.01, alpha=0.5)
+				plt.plot([i[0] for i in p2], [j[1] for j in p2], [k[2] for k in p2] , '^', color='white', lw = 0.05, markersize = 0.01, alpha=0.5)
+				plt.plot([i[0] for i in p3], [j[1] for j in p3], [k[2] for k in p3] , '^', color='blue', lw = 0.05, markersize = 0.01, alpha=0.5)
+				plt.plot([i[0] for i in p1_prime], [j[1] for j in p1_prime], [k[2] for k in p1_prime], '^', color='yellow', lw=0.05, markersize=0.01, alpha=0.5)
 
-			# 	plt.axis('on')
+				plt.axis('on')
 
-			# 	# optional: use if reference axes skeleton is desired,
-			# 	# ie plt.axis is set to 'on'
-			# 	ax.set_xticks([]), ax.set_yticks([]), ax.set_zticks([])
+				# optional: use if reference axes skeleton is desired,
+				# ie plt.axis is set to 'on'
+				ax.set_xticks([]), ax.set_yticks([]), ax.set_zticks([])
 
-			# 	# make panes have the same color as background
-			# 	ax.w_xaxis.set_pane_color((0.0, 0.0, 0.0, 1.0)), ax.w_yaxis.set_pane_color((0.0, 0.0, 0.0, 1.0)), ax.w_zaxis.set_pane_color((0.0, 0.0, 0.0, 1.0))
-			# 	ax.view_init(elev = 20, azim = i//1000)
-			# 	plt.savefig('{}'.format(i//1000), bbox_inches='tight', dpi=300)
-			# 	plt.close()
+				# make panes have the same color as background
+				ax.w_xaxis.set_pane_color((0.0, 0.0, 0.0, 1.0)), ax.w_yaxis.set_pane_color((0.0, 0.0, 0.0, 1.0)), ax.w_zaxis.set_pane_color((0.0, 0.0, 0.0, 1.0))
+				ax.view_init(elev = 20, azim = i//1000)
+				plt.savefig('{}'.format(i//1000), bbox_inches='tight', dpi=300)
+				plt.close()
 
 
-		# fig = plt.figure(figsize=(10, 10))
-		# ax = fig.gca(projection='3d')
-		# plt.gca().patch.set_facecolor('black')
+		fig = plt.figure(figsize=(10, 10))
+		ax = fig.gca(projection='3d')
+		plt.gca().patch.set_facecolor('black')
 		plt.plot([i[0] for i in p1], [j[1] for j in p1], [k[2] for k in p1] , '^', color='red', lw = 0.05, markersize = 0.01, alpha=0.5)
 		plt.plot([i[0] for i in p1_prime], [j[1] for j in p1_prime], [k[2] for k in p1_prime] , '^', color='white', lw = 0.05, markersize = 0.01, alpha=0.5)
-		# plt.plot([i[0] for i in p1], [j[1] for j in p1], [k[2] for k in p1] , '^', color='red', lw = 0.05, markersize = 0.01, alpha=0.5)
-		# plt.plot([i[0] for i in p2], [j[1] for j in p2], [k[2] for k in p2] , '^', color='white', lw = 0.05, markersize = 0.01, alpha=0.5)
-		# plt.plot([i[0] for i in p3], [j[1] for j in p3], [k[2] for k in p3] , '^', color='blue', lw = 0.05, markersize = 0.01, alpha=0.5)
+		plt.plot([i[0] for i in p1], [j[1] for j in p1], [k[2] for k in p1] , '^', color='red', lw = 0.05, markersize = 0.01, alpha=0.5)
+		plt.plot([i[0] for i in p2], [j[1] for j in p2], [k[2] for k in p2] , '^', color='white', lw = 0.05, markersize = 0.01, alpha=0.5)
+		plt.plot([i[0] for i in p3], [j[1] for j in p3], [k[2] for k in p3] , '^', color='blue', lw = 0.05, markersize = 0.01, alpha=0.5)
 
 
-		# plt.plot([i[0] for i in p1], [j[1] for j in p1], [k[2] for k in p1] , '^', color='red', lw = 0.05, markersize = 0.01, alpha=0.5)
-		# plt.plot([i[1] for i in p2], [np.sqrt(j[0]**2 + j[1]**2 + j[2]**2) for j in v2], ',', color='red', lw = 0.05, markersize = 0.01, alpha=0.8)
-		# plt.plot([i[0] for i in p3], [j[1] for j in p3], [k[2] for k in p3] , '^', color='blue', lw = 0.05, markersize = 0.01, alpha=0.5)
-		# plt.plot([i[0] for i in p2_prime], [j[1] for j in p2_prime], [k[2] for k in p2_prime], '^', color='blue', lw=0.05, markersize=0.01, alpha=0.5)
-
-		# plt.plot([i[1] for i in p2_prime], [np.sqrt(j[0]**2 + j[1]**2 + j[2]**2) for j in v2_prime], ',', color='blue', lw = 0.05, markersize = 0.01, alpha=0.8)
+		plt.plot([i[0] for i in p1], [j[1] for j in p1], [k[2] for k in p1] , '^', color='red', lw = 0.05, markersize = 0.01, alpha=0.5)
+		plt.plot([i[1] for i in p2], [np.sqrt(j[0]**2 + j[1]**2 + j[2]**2) for j in v2], ',', color='red', lw = 0.05, markersize = 0.01, alpha=0.8)
+		plt.plot([i[0] for i in p3], [j[1] for j in p3], [k[2] for k in p3] , '^', color='blue', lw = 0.05, markersize = 0.01, alpha=0.5)
+		plt.plot([i[0] for i in p2_prime], [j[1] for j in p2_prime], [k[2] for k in p2_prime], '^', color='blue', lw=0.05, markersize=0.01, alpha=0.5)
+		plt.plot([i[1] for i in p2_prime], [np.sqrt(j[0]**2 + j[1]**2 + j[2]**2) for j in v2_prime], ',', color='blue', lw = 0.05, markersize = 0.01, alpha=0.8)
 
 		plt.axis('on')
 
 		# optional: use if reference axes skeleton is desired,
 		# ie plt.axis is set to 'on'
-		# ax.set_xticks([]), ax.set_yticks([]), ax.set_zticks([])
+		ax.set_xticks([]), ax.set_yticks([]), ax.set_zticks([])
 
 		# make panes have the same color as background
-		# ax.w_xaxis.set_pane_color((0.0, 0.0, 0.0, 1.0)), ax.w_yaxis.set_pane_color((0.0, 0.0, 0.0, 1.0)), ax.w_zaxis.set_pane_color((0.0, 0.0, 0.0, 1.0))
-		# ax.view_init(elev = 20, azim = t)
-		# plt.savefig('{}'.format(t), dpi=300, bbox_inches='tight')
+		ax.w_xaxis.set_pane_color((0.0, 0.0, 0.0, 1.0)), ax.w_yaxis.set_pane_color((0.0, 0.0, 0.0, 1.0)), ax.w_zaxis.set_pane_color((0.0, 0.0, 0.0, 1.0))
+		ax.view_init(elev = 20, azim = t)
+		plt.savefig('{}'.format(t), dpi=300, bbox_inches='tight')
 		plt.show()
 		plt.close()
 
+
+
 time_steps = 50000
-t = Threebody(time_steps)
+x_res, y_res = 75, 50
+t = Threebody(time_steps, x_res, y_res)
+time_array = t.sensitivity()
 # t.three_body_trajectory()
-time_array = t.sensitivity(1000, 1500)
-# time_array = time_steps - time_array 
-# plt.style.use('dark_background')
-# plt.imshow(time_array, cmap='inferno')
-# plt.axis('off')
-# plt.savefig('Threebody_divergence{0:04d}.png'.format(3), bbox_inches='tight', pad_inches=0, dpi=410)
-# plt.show()
-# plt.close()
+time_array = time_steps - time_array 
+plt.style.use('dark_background')
+plt.imshow(time_array, cmap='inferno')
+plt.axis('off')
+plt.savefig('Threebody_divergence{0:04d}.png'.format(3), bbox_inches='tight', pad_inches=0, dpi=410)
+plt.show()
+plt.close()
 
 
 
